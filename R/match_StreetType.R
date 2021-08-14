4da0e009eadeae5a2d8eb9e84d52a19a4df1618a
HughParsonage <hugh.parsonage@gmail.com>
2021-08-14 21:09:44 +1000

    Document

--- R/match_StreetType.R
+++ R/match_StreetType.R
@@ -9,12 +9,7 @@
 #'   at which the street type starts.
 #' }
 #'
-#' @param StreetMatch2 The integer vector result of \code{match_StreetType(address, m = 2L)}.
-#' If \code{NULL} the vector is evaluated internally.
-#'
-#' @param Numbers If not \code{NULL}, then a list from \code{extract_flatNumberFirstLast}.
-#'
-#' @param hash Whether to hash the result as an integer vector.
+
 #'
 #'
 
@@ -45,24 +40,7 @@ match_StreetType_Line1 <- function(address, m = 2L) {
 }
 
 
-#' @rdname match_StreetType
-#' @export
-match_StreetName <- function(address, StreetMatch2 = NULL, Numbers = NULL, hash = FALSE) {
-  if (is.null(StreetMatch2)) {
-    StreetMatch2 <- match_StreetType(address, m = 2L)
-  }
-  jPos <- integer(0)
-  if (!is.null(Numbers) && hasName(Numbers, "j_POSITION")) {
-    jPos <- .subset2(Numbers, "j_POSITION")
-  }
 
-  .Call("Cmatch_StreetName",
-        address,
-        StreetMatch2,
-        jPos,
-        isTRUE(hash),
-        PACKAGE = packageName());
-}
 
 
 
