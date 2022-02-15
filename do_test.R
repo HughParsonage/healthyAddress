85eee55d7a0a36b67f17a7894813b67fbb8060ff
HughParsonage <hugh.parsonage@gmail.com>
2022-02-15 16:48:06 +1100

    Handle unnumbered address

--- do_test.R
+++ do_test.R
@@ -6,4 +6,13 @@
   }
 }
 
-.test()
+# .test()
+library(data.table)
+racf_lookup <- fread("~/../Downloads/racf_lookup.csv")
+setnames(racf_lookup, gsub("Physical Address ", "", names(racf_lookup)))
+setnames(racf_lookup, gsub(" +", "_", names(racf_lookup)))
+racf_lookup[, "Address" := paste0(Line_1, ", ", Suburb, " VIC ", Post_Code)]
+cat("now:\n")
+print(bench::system_time(rr <- healthyAddress::standardize_address(racf_lookup$Address)))
+bench::system_time(dr <- healthyAddress:::Do_standard_address(toupper(racf_lookup$Address)))
+
