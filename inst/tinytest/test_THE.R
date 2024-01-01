470fb82dcba3d038c8aea5bd1fda5d49dda486f8
HughParsonage <hugh.parsonage@gmail.com>
2024-01-01 23:49:50 +1100

    Fix error caused by expansion of ALPHABET
    w/out properly following through all the logic

--- inst/tinytest/test_THE.R
+++ inst/tinytest/test_THE.R
@@ -4,3 +4,9 @@ get_the_XXX <- healthyAddress:::get_the_XXX
 expect_equal(the__("ABC"), 0)
 expect_equal(the__("THE"), 0)
 expect_equal(healthyAddress:::the__("THE GAP"), match("GAP", get_the_XXX(), nomatch = 0L))
+
+expect_equal(the__(c("1/1 THE RIGHI SOUTH YARRA VIC 3000",
+                     "2/2 THE OAKS THE GAP")))
+
+expect_equal(the__("90 THE OLD STOCK RUN, SUNBURY, VIC, 3429"),
+             match("OLD STOCK RUN", get_the_XXX(), nomatch = 0L))
