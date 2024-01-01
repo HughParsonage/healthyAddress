2fbef4ad35b09e7fb0916ddcd8933c391c9bc14e
HughParsonage <hugh.parsonage@gmail.com>
2024-01-02 00:15:19 +1100

    fix empty test

--- inst/tinytest/test_THE.R
+++ inst/tinytest/test_THE.R
@@ -6,7 +6,8 @@ expect_equal(the__("THE"), 0)
 expect_equal(healthyAddress:::the__("THE GAP"), match("GAP", get_the_XXX(), nomatch = 0L))
 
 expect_equal(the__(c("1/1 THE RIGHI SOUTH YARRA VIC 3000",
-                     "2/2 THE OAKS THE GAP")))
+                     "2/2 THE OAKS THE GAP")),
+             match(c("RIGHI", "OAKS"), get_the_XXX(), nomatch = 0L))
 
 expect_equal(the__("90 THE OLD STOCK RUN, SUNBURY, VIC, 3429"),
              match("OLD STOCK RUN", get_the_XXX(), nomatch = 0L))
