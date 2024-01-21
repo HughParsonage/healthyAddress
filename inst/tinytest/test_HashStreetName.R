0b3aaafa2d9f58bc1b5db36a0a4e9ac03d2b0479
HughParsonage <hugh.parsonage@gmail.com>
2024-01-22 00:55:24 +1100

    test unHash

--- inst/tinytest/test_HashStreetName.R
+++ inst/tinytest/test_HashStreetName.R
@@ -5,4 +5,4 @@ expect_equal(HashStreetName(c("BRUXNER", "AGAR STEPS", NA)),
              c(1788253569L, 2122551793L, NA))
 expect_error(HashStreetName(1L), "integer")
 
-# expect_equal(unHashStreetName(HashStreetName("THE PARKWAY")), "THE PARKWAY")
+expect_equal(unHashStreetName(HashStreetName("THE PARKWAY")), "THE PARKWAY")
