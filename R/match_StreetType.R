518f373d88dc3790f402cf3f80dc107669515c1e
HughParsonage <hugh.parsonage@gmail.com>
2024-02-02 22:18:30 +1100

    Coverage, update docs

--- R/match_StreetType.R
+++ R/match_StreetType.R
@@ -1,35 +1,24 @@
 #' Find the street type within an address
 #' @param address A character vector, every string an address.
-#' @param m An integer. Values greater than \code{0} are for internal use and
-#' encode additional data in the return value:
-#'
-#' \enumerate{
-#'   \item An integer vector \code{y} is returned such that \code{y mod 256} gives
-#'   which street code and \code{y / 256} gives the character position in address
-#'   at which the street type starts.
-#' }
-#'
-
 #'
 #'
-
-#'
-#'
-#' @return Which of \code{\link{.permitted_street_type_ord}} appears
-#' in \code{address}.
+#' @return A list of two elements. The first element are the indices of
+#' street type in \code{.permitted_street_type_ord()} that is found in the
+#' address. The second element are the corresponding string positions of
+#' the street so identified.
 #'
 #' @examples
 #' cds <- .permitted_street_type_ord()
 #' head(cds)
 #' match_StreetType("712 FLINDERS STREET MELBOURNE 3004")
-#' match_StreetType("712 FLINDERS STREET MELBOURNE 3004", 1L)
+#' #                 012345678901234
+#' match_StreetType("712 FLINDERS ST MELBOURNE 3004")
 #'
 #' @export
 
-match_StreetType <- function(address, m = 0L) {
-  stopifnot(is.character(address), is.integer(m))
-  Ans <- match_StreetType_trie(address)
-
+match_StreetType <- function(address) {
+  stopifnot(is.character(address))
+  Ans <- match_StreetType_trie(ensure_uppercase(address))
 }
 
 match_StreetType_Line1 <- function(address, m = 2L) {
