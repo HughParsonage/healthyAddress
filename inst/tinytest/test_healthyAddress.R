29f1e85f076e3b2b01d49e1c33b0ec1ed4bffe5f
HughParsonage <hugh.parsonage@gmail.com>
2024-02-11 13:04:48 +1100

    Update postcodes < 3000

--- inst/tinytest/test_healthyAddress.R
+++ inst/tinytest/test_healthyAddress.R
@@ -58,6 +58,10 @@ expect_equal(ans$NUMBER_LAST, 5)
 ans <- standardize_address("1 THE PINES AVENUE SYMONSTON ACT 2609")
 expect_equal(ans$NUMBER_FIRST, 1L)
 
+ans <- standardize_address(c("9 MONSOON RD DARWIN NT 0810",
+                             "19 CYCAD CIRCUIT DARWIN NT 0810"))
+expect_equal(ans$STREET_NAME, c("MONSOON", "CYCAD"))
+
 ans <- standardize_address(c("15 CANBERRA AVENUE FORREST ACT 2609",
                              "1 BEALE CRESCENT DEAKIN ACT 2600",
                              "116 MUGGA LANE Symonston ACT 2609",
