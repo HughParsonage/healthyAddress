c596cd8b050a415f7852c07022611a265b67694c
HughParsonage <hugh.parsonage@gmail.com>
2024-02-11 14:06:20 +1100

    Add new postcode

--- inst/tinytest/test_is_postcode.R
+++ inst/tinytest/test_is_postcode.R
@@ -1,3 +1,4 @@
 expect_true(healthyAddress:::isPostcode(2000L) & TRUE)
 expect_false(healthyAddress:::isPostcode(NA_integer_) & TRUE)
-expect_equal(healthyAddress:::isPostcode(4513), 1L) # new postcode
+expect_equal(healthyAddress:::isPostcode(4513L), 1L) # new postcode
+expect_equal(healthyAddress:::isPostcode(6219L), 1L) # new postcode
