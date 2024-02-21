74c45b158a06e9aa8ee4dec34c1f6a62ebc9a850
HughParsonage <hugh.parsonage@gmail.com>
2024-02-21 22:55:30 +1100

    Export compress_latlon

--- R/latlon.R
+++ R/latlon.R
@@ -1,13 +1,41 @@
-
+#' Compress latitude and longitude to a 32-bit integer
+#' @description Although lat and lon are represented by doubles, this is usually
+#' slightly wasteful. This function allows you to represent coordinates as single
+#' integer, vastly reducing memory footprint.
+#' @param lat,lon Coordinates to compress.
+#' @param nThread Number of threads to use.
+#' @param x An integer vector formed by one of the compression functions.
+#'
+#' @return
+#' The `_general` version of the compression/decompression use the observed
+#' range of the latitude and longitude to form a \eqn{2^16} grid, while the
+#' bare versions use the known limits of Australian address coordinates
+#' (including the overseas territories). Since, in the latter, the grid
+#' will be much less fine, you should expect greater loss of information,
+#' possibly exceeding 100 metres.
+#'
+#'
+#' \describe{
+#' \item{\code{compress_latlon}}{An integer vector.}
+#' \item{\code{decompress_latlon}}{The original \code{lat,lon}, with some information loss}
+#' \item{\code{compress_latlon_general}}{An integer vector, with attributes `minmaxLat` and `minmaxLon`.}
+#' \item{\code{decompress_latlon_general}}{The original \code{lat,lon}, with some information loss.}
+#' }
+#'
+#' @export
 
 compress_latlon <- function(lat, lon, nThread = getOption("healthyAddress.nThread", 1L)) {
   .Call("C_compress_latlon", lat, lon, nThread, PACKAGE = packageName())
 }
 
+#' @rdname compress_latlon
+#' @export
 decompress_latlon <- function(x, nThread = getOption("healthyAddress.nThread", 1L)) {
   .Call("C_decompress_latlon", x, nThread, PACKAGE = packageName())
 }
 
+#' @rdname compress_latlon
+#' @export
 compress_latlon_general <- function(lat, lon, nThread = getOption("healthyAddress.nThread", 1L)) {
   minmaxLat <- minmax(lat)
   minmaxLon <- minmax(lon)
@@ -20,6 +48,8 @@ compress_latlon_general <- function(lat, lon, nThread = getOption("healthyAddres
   ans
 }
 
+#' @rdname compress_latlon
+#' @export
 decompress_latlon_general <- function(x, nThread = getOption("healthyAddress.nThread", 1L)) {
   stopifnot(is.integer(x))
   minmaxLat <- attr(x, "minmaxLat")
